6447683b06b646c810a5301cdc7000e292470798
Joel Valerio <88332213+JoelV304@users.noreply.github.com>
2021-08-12 17:11:03 +0000

    Animated underline links

--- script.js
+++ script.js
@@ -5,43 +5,41 @@ function hoverShrink() {
   var shrink = document.getElementById('grow').style.width = "10%"
 }
 function getFormValue() {
-  var fname = document.getElementById('fname').value; document.getElementById('nameDisplay').innerHTML = 'Hey, ' + fname;
+  var fname = document.getElementById('fname').value;  document.getElementById('nameDisplay').innerHTML = 'Hey, ' + fname;
   var formDel = document.getElementById("form");
   formDel.remove();
   var buttonDel = document.getElementById("subbutton");
   buttonDel.remove();
 }
 function linkGrow() {
-  var linkGrow = document.getElementById('linkedin').style.width = '5%'
-  var linkGrow = document.getElementById('linkedin').style.height = '5%'
+  var linkGrow= document.getElementById('linkedin').style.width= '5%'
+  var linkGrow= document.getElementById('linkedin').style.height= '5%'
 }
 function linkShrink() {
-  var linkShrink = document.getElementById('linkedin').style.width = '4%'
-  var linkShrink = document.getElementById('linkedin').style.height = '4%'
+  var linkShrink= document.getElementById('linkedin').style.width= '4%'
+  var linkShrink= document.getElementById('linkedin').style.height= '4%'
 }
 function youGrow() {
-  var youGrow = document.getElementById('youtube').style.width = '6.5%'
-  var youGrow = document.getElementById('youtube').style.height = '6.5%'
+  var youGrow= document. getElementById('youtube').style.width= '6.5%'
+  var youGrow= document. getElementById('youtube').style.height= '6.5%'
 }
 function youShrink() {
-  var youShrink = document.getElementById('youtube').style.width = '5.5%'
-  var youShrink = document.getElementById('youtube').style.height = '5.5%'
+  var youShrink= document.getElementById('youtube').style.width= '5.5%'
+  var youShrink= document.getElementById('youtube').style.height= '5.5%'
 }
 function instaGrow() {
-  var instaGrow = document.getElementById('insta').style.width = '6%'
-  var instaGrow = document.getElementById('insta').style.height = '6%'
+  var instaGrow= document.getElementById('insta').style.width= '6%'
+  var instaGrow= document.getElementById('insta').style.height= '6%'
 }
 function instaShrink() {
-  var instaShrink = document.getElementById('insta').style.width = '5%'
-  var instaShrink = document.getElementById('insta').style.height = '5%'
+  var instaShrink= document.getElementById('insta').style.width= '5%'
+  var instaShrink= document.getElementById('insta').style.height= '5%'
 }
 function getQuizValue() {
-  var answer = document.getElementById('fitness');
-  console.log(answer)
-  if (answer == 'fitness') {
+  var answer = document.getElementById('fitness').checked;
+  if ('fitness'.checked){
     document.getElementById('results').innerHTML = 'You got question 1 correct!!'
-  } else {
+  }else{
     document.getElementById('results').innerHTML = 'Guess again on question 1!!'
   }
-
 }
